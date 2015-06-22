f11548ca1b5db75102561616a5b16a1605143647
teeteetee <proto@protos-MacBook-Pro.local>
2015-06-22 05:19:06 -0700

    --

--- app.js
+++ app.js
@@ -27,17 +27,17 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.compress());
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 2540000000 }));
-app.use(sessions({
-  cookieName: 'session',
-  secret:'2342kjhkj2h3i2uh32j3hk2jDKLKSl23kh42u3ih4',
-  duration:4320 * 60 *1000,
-  activeduration:1440 * 60 * 1000,
-  cookie: {
-    path:'/',
-  httpOnly: true,
-  //domain:'.vntrlst.com'
-  }
-}));
+//app.use(sessions({
+//  cookieName: 'session',
+//  secret:'2342kjhkj2h3i2uh32j3hk2jDKLKSl23kh42u3ih4',
+//  duration:4320 * 60 *1000,
+//  activeduration:1440 * 60 * 1000,
+//  cookie: {
+//    path:'/',
+//  httpOnly: true,
+//  //domain:'.vntrlst.com'
+//  }
+//}));
 
 //SUBDOMAIN MAGIC 
 
